0b04cef5099d8f7c868b6bae00834bdc0978b718
y2Dev <dub-y@tuta.io>
2022-10-03 10:26:49 -0400

    création des fonctions express et app

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express') ;
+
+// La fonction app reconnaitra toutes les informations de Express
+const app = express();
+
+
+// Se connecter au serveur
+// écoute tout ce qui ce passe au port 5500
+app.listen(5500, () => console.log('Server started: 5500'));
