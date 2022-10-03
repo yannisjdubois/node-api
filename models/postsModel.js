ba2b590dcec89e367338509a29baedc5cf191324
y2Dev <dub-y@tuta.io>
2022-10-03 12:33:50 -0400

    Création d'un modèle de base de donnée réutilisable

---
+++ models/postsModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// Création d'un modèle de base de donnée réutilisable
+// La majuscule (P) est une convention
+const PostsModel = mongoose.model(
+    "node-api",
+    {
+        author: {
+            type: String,
+            require: true // active le champs
+        },
+        message: {
+            type: String,
+            require: true // active le champs
+        }
+    },
+    "posts"
+)
