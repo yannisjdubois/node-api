add6a16136ab36c525980ff67f23d1b220875eef
y2Dev <dub-y@tuta.io>
2022-10-04 16:26:58 -0400

    Création de la méthode PUT

--- routes/postsController.js
+++ routes/postsController.js
@@ -1,5 +1,6 @@
 const express = require('express') ;
 const router = express.Router() ;
+const ObjectID = require('mongoose').Types.ObjectId; // récupère Identifiant de l'objet
 
 const { PostsModel } = require('../models/postsModel')
 
@@ -24,6 +25,27 @@ router.post('/', (req, res) => {
         if (!err) res.send(docs);
         else console.log("Error creating new data :" + err);
     })
+});
+
+// Méthode PUT pour modifier données
+router.put("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknow : " + req.params.id)
+
+    const updateRecord = {
+        author: req.body.author,
+        message: req.body.message
+    };
+
+    PostsModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updateRecord},
+        { new: true},
+        (err, docs) => {
+            if (!err) res.send(docs)
+            else console.log("Update error : " + err);
+        }
+    )
 })
 
 module.exports = router
